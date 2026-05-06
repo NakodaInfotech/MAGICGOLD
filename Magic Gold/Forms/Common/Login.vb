388ed49ac53eeee5e3c3d36ad833bb2e86140468
NakodaInfotech <sales@nakodainfotech.com>
2026-05-06 15:07:48 +0530

    CHANGES

--- Magic Gold/Forms/Common/Login.vb
+++ Magic Gold/Forms/Common/Login.vb
@@ -135,7 +135,7 @@ Public Class LoginForm
                     GoTo LINE1
                 End If
             ElseIf ClientName = "SHASHAWAT" Then '(SUSHIL BHAI)
-                If Now.Date > DateTime.Parse("15.05.2026 00:00") Then
+                If Now.Date > DateTime.Parse("15.05.2027 00:00") Then
                     tempcmd = New OleDbCommand("UPDATE VERSION SET VERSION_NO=1.0.0000", tempconn)
                     tempcmd.ExecuteNonQuery()
                     GoTo LINE1
