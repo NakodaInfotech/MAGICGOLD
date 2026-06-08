bc3bca4349fdfe27af1a2b6a06c0425824c3bdba
Gulkit <sales@nakodainfotech.com>
2026-06-08 12:21:21 +0530

    changes

--- Magic Gold/Forms/Common/Login.vb
+++ Magic Gold/Forms/Common/Login.vb
@@ -74,7 +74,7 @@ Public Class LoginForm
                 '        GoTo LINE1
                 '    End If
             ElseIf ClientName = "GAUTAM" Then
-                If Now.Date > DateTime.Parse("15.06.2026 00:00") Then
+                If Now.Date > DateTime.Parse("15.06.2027 00:00") Then
                     tempcmd = New OleDbCommand("UPDATE VERSION SET VERSION_NO=1.0.0000", tempconn)
                     tempcmd.ExecuteNonQuery()
                     GoTo LINE1
