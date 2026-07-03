7d7c8d56454a4c8b0838e9efa35c5436135fffb3
Moin <sales@nakodainfotech.com>
2026-07-03 15:29:40 +0530

    CHANGES

--- Magic Gold/Forms/Common/Login.vb
+++ Magic Gold/Forms/Common/Login.vb
@@ -41,7 +41,7 @@ Public Class LoginForm
                 '        GoTo LINE1
                 '    End If
             ElseIf ClientName = "CHAINCRAFTS" Then  'GIRISH BHAI CHENNAI)
-                If Now.Date > DateTime.Parse("15.06.2026 00:00") Then
+                If Now.Date > DateTime.Parse("15.07.2027 00:00") Then
                     tempcmd = New OleDbCommand("UPDATE VERSION SET VERSION_NO=1.0.0000", tempconn)
                     tempcmd.ExecuteNonQuery()
                     GoTo LINE1
